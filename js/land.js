d542feb76adf02a95ff6728521d38e2d4cefd67c
Darthpool9900 <rafael.zachesky@gmail.com>
2022-10-16 17:01:51 -0300

    V 1.1.0

--- js/land.js
+++ js/land.js
@@ -10,7 +10,7 @@ var list_request = document.querySelectorAll('.mobile ul li a');
 var x = 0;
 
 prev.addEventListener('click',(e)=>{
-        x = x - 400;//Remove 80 da coordenada x
+        x = x - 400;//Remove 4000 da coordenada x
         
         if(x<0){//Verifica tamanho minimo de x
             x = 0;
@@ -19,8 +19,8 @@ prev.addEventListener('click',(e)=>{
 })
 
 
-after.addEventListener('click',(e)=>{//Adiciona 80 a coordenada x
-        x = x +400;
+after.addEventListener('click',(e)=>{
+        x = x +400;//Adiciona 4000 a coordenada x
         
         if(x>(400*3.2)){//Verifica o tamanho máxmio de x
             x = (400*3.2);
@@ -30,33 +30,33 @@ after.addEventListener('click',(e)=>{//Adiciona 80 a coordenada x
 
 
 mobile_select.addEventListener('click',(e)=>{
-        mobile_menu.style.display = 'block';
-        setTimeout(()=>{
-            mobile_menu.style.opacity ='1';
+        mobile_menu.style.display = 'block';//seleciona o elemento e estilo no html
+        setTimeout(()=>{//define delay da animação
+            mobile_menu.style.opacity ='1';//configura a opacidade para 1
         },050);
         
-        mobile_select.style.display = 'none';
+        mobile_select.style.display = 'none';//configura o display para o elemento não aparecer na tela
 })
 
 close.addEventListener('click',(e)=>{
-    if(close==e.target){
-        mobile_menu.style.display = 'none';
+    if(close==e.target){//Verifica se o elemento da condição é alvo do cursor
+        mobile_menu.style.display = 'none';//seleciona o elemento e estilo no html
         setTimeout(()=>{
-            mobile_menu.style.opacity ='0';
+            mobile_menu.style.opacity ='0';//configura a opacidade para 0
         },500);
-        mobile_select.style.display = 'block';
+        mobile_select.style.display = 'block';//configura o display para o elemento aparecer na tela
     }
 })
 
-for(var i=0;i<list_request.length;i++){
-    list_request[i].addEventListener('click',(e)=>{
-        for(var j=0;j<list_request.length;j++){
-            setTimeout(()=>{
-                mobile_menu.style.opacity ='0';
+for(var i=0;i<list_request.length;i++){//Inicio fluxo de repetição para selecionar o array de elementos
+    list_request[i].addEventListener('click',(e)=>{//Adiciona evento de clique
+        for(var j=0;j<list_request.length;j++){//Segundo fluxo para aplicar estilização nos elementos
+            setTimeout(()=>{//define delay da animação
+                mobile_menu.style.opacity ='0';//configura a opacidade para 0
             },500);
-        if(list_request[j]==e.target){
-            mobile_menu.style.display = 'none';
-            mobile_select.style.display = 'block';
+        if(list_request[j]==e.target){//Verifica se o elemento da condição é alvo do cursor
+            mobile_menu.style.display = 'none';//configura o display para o elemento não aparecer na tela
+            mobile_select.style.display = 'block';//configura o display para o elemento  aparecer na tela
         }
     }
     })
