afbc9da20a85536926a59147005c7c6310784b7b
Darthpool9900 <rafael.zachesky@gmail.com>
2022-10-16 16:02:29 -0300

    V 1.0.3

--- js/land.js
+++ js/land.js
@@ -2,6 +2,11 @@ var prev = document.querySelector('.prev');
 var after = document.querySelector('.after');
 var slide = document.querySelector('.flex_display');
 var  submit = document.querySelector('.email');
+var modal = document.querySelector('.thanks');
+var mobile_select = document.querySelector('.mobile_menu');
+var mobile_menu = document.querySelector('.mobile');
+var close = document.querySelector('.close');
+var list_request = document.querySelectorAll('.mobile ul li a');
 var x = 0;
 
 prev.addEventListener('click',(e)=>{
@@ -23,22 +28,37 @@ after.addEventListener('click',(e)=>{//Adiciona 80 a coordenada x
         slide.scroll(x,0);//Desloca a scrollbar no eixo X
 })
 
-submit.addEventListener('click',()=>{
-fetch("https://formsubmit.co/ajax/your@email.com", {
-    method: "POST",
-    headers: { 
-        'Content-Type': 'application/json',
-        'Accept': 'application/json'
-    },
-    body: JSON.stringify({
-        name: "",
-        message: ""
-    })
+
+mobile_select.addEventListener('click',(e)=>{
+        mobile_menu.style.display = 'block';
+        setTimeout(()=>{
+            mobile_menu.style.opacity ='1';
+        },050);
+        
+        mobile_select.style.display = 'none';
 })
-    .then(response => response.json())
-    .then(data => console.log(data))
-    .catch(error => console.log(error));
-});
 
+close.addEventListener('click',(e)=>{
+    if(close==e.target){
+        mobile_menu.style.display = 'none';
+        setTimeout(()=>{
+            mobile_menu.style.opacity ='0';
+        },500);
+        mobile_select.style.display = 'block';
+    }
+})
 
+for(var i=0;i<list_request.length;i++){
+    list_request[i].addEventListener('click',(e)=>{
+        for(var j=0;j<list_request.length;j++){
+            setTimeout(()=>{
+                mobile_menu.style.opacity ='0';
+            },500);
+        if(list_request[j]==e.target){
+            mobile_menu.style.display = 'none';
+            mobile_select.style.display = 'block';
+        }
+    }
+    })
+}
 
