9a3c9af7d696a1d81354de8a9ad8ba42e0b7838a
Darthpool9900 <rafael.zachesky@gmail.com>
2023-01-06 12:58:56 -0300

    New interface

--- js/land.js
+++ js/land.js
@@ -1,63 +1,5 @@
 $(document).ready(function () {
    $('.loading').css('display', 'none');
-   
-
-<<<<<<< HEAD
-prev.addEventListener('click',(e)=>{
-        x = x - slide.offsetWidth;//Remove 4000 da coordenada x
-        
-        if(x<0){//Verifica tamanho minimo de x
-            x = 0;
-        }
-        slide.scroll(x,0);//Desloca a scrollbar no eixo X
-})
-
-
-after.addEventListener('click',(e)=>{
-        x = x +slide.offsetWidth;//Adiciona 4000 a coordenada x
-        
-        if(x>(400*3)&&window.innerWidth>440){//Verifica o tamanho máxmio de x
-            x = (400*3);
-        }else if(x>(400*3)&&window.innerWidth<=440){
-            x = slide.offsetWidth;
-        }
-        slide.scroll(x,0);//Desloca a scrollbar no eixo X
-})
-
-
-mobile_select.addEventListener('click',(e)=>{
-        mobile_menu.style.display = 'block';//seleciona o elemento e estilo no html
-        setTimeout(()=>{//define delay da animação
-            mobile_menu.style.opacity ='1';//configura a opacidade para 1
-        },050);
-        
-        mobile_select.style.display = 'none';//configura o display para o elemento não aparecer na tela
-})
-
-close.addEventListener('click',(e)=>{
-    if(close==e.target){//Verifica se o elemento da condição é alvo do cursor
-        mobile_menu.style.display = 'none';//seleciona o elemento e estilo no html
-        setTimeout(()=>{
-            mobile_menu.style.opacity ='0';//configura a opacidade para 0
-        },500);
-        mobile_select.style.display = 'block';//configura o display para o elemento aparecer na tela
-    }
-})
-
-for(var i=0;i<list_request.length;i++){//Inicio fluxo de repetição para selecionar o array de elementos
-    list_request[i].addEventListener('click',(e)=>{//Adiciona evento de clique
-        for(var j=0;j<list_request.length;j++){//Segundo fluxo para aplicar estilização nos elementos
-            setTimeout(()=>{//define delay da animação
-                mobile_menu.style.opacity ='0';//configura a opacidade para 0
-            },500);
-        if(list_request[j]==e.target){//Verifica se o elemento da condição é alvo do cursor
-            mobile_menu.style.display = 'none';//configura o display para o elemento não aparecer na tela
-            mobile_select.style.display = 'block';//configura o display para o elemento  aparecer na tela
-        }
-    }
-    })
-}
-=======
    $('.btn_hidden').click(function () {
       $('.mobile').css('display', 'block');
       $('.mobile').css('width','100%');
@@ -65,8 +7,6 @@ for(var i=0;i<list_request.length;i++){//Inicio fluxo de repetição para seleci
          $('.menu_mobile li a').css('opacity','1');
       },500)
    })
->>>>>>> alpha
-
    $('.close').click(function () {
       $('.menu_mobile li a').css('opacity','0');
       $('.mobile').css('width','0%');
